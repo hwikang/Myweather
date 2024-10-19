b63db7d5aba86e93eefdb21bebe4a1977de7d9f0
hwikang <khdrogba@gmail.com>
2024-10-19 10:47:52 +0900

    UI 코드 분리

--- MyWeather/Projects/App/Sources/Main/MainView.swift
+++ MyWeather/Projects/App/Sources/Main/MainView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 import Network
 import MapKit
+import Entity
 
 public struct MainView: View {
     @ObservedObject private var viewModel: MainViewModel
@@ -19,65 +20,15 @@ public struct MainView: View {
     
     public var body: some View {
         
-
        if let data = viewModel.weatherData {
            ZStack {
                ScrollView(.vertical, showsIndicators: true) {
                    VStack {
-                       Button(action: {
-                           presentSearchView = true
-                       }, label: {
-                           HStack {
-                               Image(systemName: "magnifyingglass")
-                               Text("Search")
-                               Spacer()
-                           }
-                       })
-                       .foregroundStyle(.gray)
-                       .frame(height: 44)
-                       .overlay(
-                           RoundedRectangle(cornerRadius: 6)
-                               .stroke(Color.gray, lineWidth: 1)
-                       )
-                       .padding(16)
-                       
-                       VStack {
-                           if let firstDayWeatehr = data.dailyWeathers.first, let firstDayWeatherDetail = firstDayWeatehr.weather.first {
-                               Text(data.timezone)
-                               Text(firstDayWeatehr.dayTemp.tempFormatted())
-                                   .font(.title)
-                                   .fontWeight(.bold)
-                               Text(firstDayWeatherDetail.description)
-                                   .font(.subheadline)
-                               Text("최고: \(firstDayWeatehr.maxTemp.tempFormatted()) 최저: \(firstDayWeatehr.minTemp.tempFormatted())")
-                               
-                           }
-                       }
-                       ScrollView(.horizontal, showsIndicators: false) {
-                           HStack {
-                               ForEach(data.hourlyWeathers, id: \.dt) { item in
-                                   
-                                   VStack{
-                                       Text(item.dt.koranHourFormatted())
-                                       if let icon = item.weather.first?.icon.prefix(2) {
-                                           Image("\(String(icon))d")
-                                       }
-                                       
-                                       Text(item.temp.tempFormatted())
-                                   }
-                               }
-                           }
-                       }
-                       ForEach(data.dailyWeathers, id: \.dt) { item in
-                           HStack {
-                               Text(item.dt.koranDayFormatted())
-                               if let icon = item.weather.first?.icon.prefix(2) {
-                                   Image("\(String(icon))d")
-                               }
-                               Text("최저: \(item.minTemp.tempFormatted())")
-                               Text("최고: \(item.maxTemp.tempFormatted())")
-                           }
-                       }
+                       SearchButton(presentSearchView: $presentSearchView)
+                       CurrentWeatherView(data: data)
+                       HourlyWeatherView(hourlyWeathers: data.hourlyWeathers)
+                       DailyWeatherView(dailyWeathers: data.dailyWeathers)
+                     
                        MapView(coordinate: .init(latitude: data.lat, longitude: data.lon))
                            .frame(maxWidth: .infinity, minHeight: 300)
                            .cornerRadius(10)
@@ -104,6 +55,84 @@ public struct MainView: View {
            Text("Loading...")
        }
    }
+    
+}
+
+struct SearchButton: View {
+    @Binding var presentSearchView: Bool
+
+    var body: some View {
+        Button(action: {
+            presentSearchView = true
+        }, label: {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                Text("Search")
+                Spacer()
+            }
+        })
+        .foregroundStyle(.gray)
+        .frame(height: 44)
+        .overlay(
+            RoundedRectangle(cornerRadius: 6)
+                .stroke(Color.gray, lineWidth: 1)
+        )
+        .padding(16)
+    }
+}
+
+struct CurrentWeatherView: View {
+    let data: WeatherResult
+
+    var body: some View {
+        VStack {
+            if let firstDayWeather = data.dailyWeathers.first, let firstDayWeatherDetail = firstDayWeather.weather.first {
+                Text(data.timezone)
+                Text(firstDayWeather.dayTemp.tempFormatted())
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text(firstDayWeatherDetail.description)
+                    .font(.subheadline)
+                Text("최고: \(firstDayWeather.maxTemp.tempFormatted()) 최저: \(firstDayWeather.minTemp.tempFormatted())")
+            }
+        }
+    }
+}
+
+struct HourlyWeatherView: View {
+    let hourlyWeathers: [HourlyWeather]
+
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(hourlyWeathers, id: \.dt) { item in
+                    VStack {
+                        Text(item.dt.koranHourFormatted())
+                        if let icon = item.weather.first?.icon.prefix(2) {
+                            Image("\(String(icon))d")
+                        }
+                        Text(item.temp.tempFormatted())
+                    }
+                }
+            }
+        }
+    }
+}
+struct DailyWeatherView: View {
+    let dailyWeathers: [DailyWeather]
+
+    var body: some View {
+        ForEach(dailyWeathers, id: \.dt) { item in
+            HStack {
+                Text(item.dt.koranDayFormatted())
+                if let icon = item.weather.first?.icon.prefix(2) {
+                    Image("\(String(icon))d")
+                }
+                Text("최저: \(item.minTemp.tempFormatted())")
+                Text("최고: \(item.maxTemp.tempFormatted())")
+            }
+        }
+    }
 }
 
 struct WeatherContainerView: View {
